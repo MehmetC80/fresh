3bd6ddb0f012675bb359b2b53f1931da8cc6dba5
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-07-22 09:26:52 +0200

    cleanup project, add folder structure,add images

---
+++ src/components/notification.tsx
@@ -0,0 +1,8 @@
+export const Notification = () => {
+  return (
+    <div className='h-12 bg-orange-500 text-white text-center flex justify-center items-center text-sm md:text-base cursor-pointer'>
+      Gratis Lieferung allr Produkte ab einer Bestellung von 40€. Bestelle
+      gleich dein Essen! px-4
+    </div>
+  );
+};
